283da884731a494010fedfe0385b27bb00c556ac
kocabiyik703 <kocabiyik703@gmail.com>
2025-09-04 18:24:25 +0300

    Update aiAssistant.js

--- src/js/ui/aiAssistant.js
+++ src/js/ui/aiAssistant.js
@@ -64,7 +64,7 @@ class AIAssistant {
         this.enableLegacyEvents = false;
         
         //this.apiKey = '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'; // OpenRouter API anahtarı (DeepSeek)
-        this.apiKey =  '';
+        this.apiKey =  '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';
         // Chat geçmişi
         this.chatHistory = [];
     }
